b828e4cf802f5657d68416aea4cc4ebc3fcf4f35
Jonathan <jonafanho@gmail.com>
2019-06-19 00:49:13 -0700

    Merged overlapping stops for stops along the 80x and 90x.

--- update.bat
+++ update.bat
@@ -1,7 +1,21 @@
+del /q /f GTFS\*.txt
+
 curl https://raw.githubusercontent.com/whatcomtrans/publicwtadata/master/GTFS/wta_gtfs_latest.zip > GTFS/WTA.zip
 curl http://gtfs.s3.amazonaws.com/skagit-transit_20150429_1915.zip > GTFS/Skagit_Transit.zip
 curl https://raw.githubusercontent.com/transitland/gtfs-archives-not-hosted-elsewhere/master/island-transit-wa-us.zip > GTFS/Island_Transit.zip
 curl http://mjcaction.com/MJC_GTFS_Public/jefferson_google_transit.zip > GTFS/Jefferson_Transit.zip
+
+GTFS\7za e GTFS/Skagit_Transit.zip -oGTFS
+
+powershell -Command "gci GTFS/*.txt -recurse | ForEach { (Get-Content $_ | ForEach {$_ -replace '80X - N3', '80X-N3'}) | Set-Content $_ }"
+powershell -Command "gci GTFS/*.txt -recurse | ForEach { (Get-Content $_ | ForEach {$_ -replace '80X - N4', '80X-N4'}) | Set-Content $_ }"
+powershell -Command "gci GTFS/*.txt -recurse | ForEach { (Get-Content $_ | ForEach {$_ -replace '80X - N5', '80X-N5'}) | Set-Content $_ }"
+powershell -Command "gci GTFS/*.txt -recurse | ForEach { (Get-Content $_ | ForEach {$_ -replace '90X -N1', '90X-N1'}) | Set-Content $_ }"
+
+GTFS\7za a GTFS/Skagit_Transit.zip ./GTFS/*.txt
+
+del /q /f GTFS\*.txt
+
 GTFS\7za a GTFS/Skagit_Transit.zip ./GTFS/Patches/*
 GTFS\7za a GTFS/Island_Transit.zip ./GTFS/Patches/*
 pause
